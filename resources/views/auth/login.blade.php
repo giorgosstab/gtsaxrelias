39ebe2d5877e9b4d86eb2a69440a71717f660220
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-09-19 17:00:53 +0300

    Fix Auth Login View

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,105 @@
-@extends('layouts.app')
+@extends('portfolio.main-dark')
+
+@section('title', 'Home')
+
+@section('extra-css')
+
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <!-- Start Account Page Section -->
+    <section class="account">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-6 content">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                    <!-- Left Title -->
+                    <h1 class="left-title">Hello<br />World.</h1>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                    <!-- Left Description -->
+                    <p class="left-description">Lorem ipsum dolor sit amet, consecteturadipisicing elitsed do eiusmod tempor...</p>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <!-- Login With Social Title -->
+                    <p class="social">Login With Social Media</p>
+
+                    <!-- Facebook Login -->
+                    <a class="facebook" href="#"><i class="fab fa-facebook-f"></i></a>
+
+                    <!-- Twitter Login -->
+                    <a class="twitter" href="#"><i class="fab fa-twitter pr-2"></i>Login With Twitter</a>
+                </div>
+
+                <div class="col-12 col-lg-6 m-auto">
+
+                    <div class="form">
+
+                        <!-- Right Title -->
+                        <p class="right-title">Login</p>
+
+                        <!-- Right Description -->
+                        <p class="right-description">Lorem ipsum dolor sit amet, consecteturadipisicing elitsed do eiusmod tempor...</p>
+                        <form class="form-group" method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <!-- Login Input Item -->
+                            <div class="wrapper">
+
+                                <!-- Login Input Icon -->
+                                <div class="icon">
+                                    <i class="fas fa-envelope"></i>
+                                </div>
+
+                                <!-- Login Input -->
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback" role="alert" style="margin-top: -8px;margin-bottom: 10px;">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <!-- Login Input Item -->
+                            <div class="wrapper">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <!-- Login Input Icon -->
+                                <div class="icon">
+                                    <i class="fas fa-lock"></i>
+                                </div>
+
+                                <!-- Login Input -->
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback" role="alert" style="margin-top: -8px;margin-bottom: 10px;">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <div class="clearfix">
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                <!-- Remember Me-->
+                                <div class="custom-control custom-checkbox">
+                                    <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <label class="custom-control-label align-middle" for="remember">{{ __('Remember Me') }}</label>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
+                                <!-- Forget Password Link -->
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="forget" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
                             </div>
-                        </div>
-                    </form>
+
+                            <!-- Sign in Button -->
+                            <button type="submit" class="button btn-three text-uppercase">Sign In</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
+    <!-- End Account Page Section -->
 @endsection
