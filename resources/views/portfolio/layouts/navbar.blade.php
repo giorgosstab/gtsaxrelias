28a6b0a4070555f4ff551df9e11b5c1a9b446eb9
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-10-13 15:11:28 +0300

    Change only navbar light on home page

--- resources/views/portfolio/layouts/navbar.blade.php
+++ resources/views/portfolio/layouts/navbar.blade.php
@@ -1,10 +1,10 @@
 <!-- Start Navigation Bar -->
-<nav class="navbar navbar-expand-lg @if (Request::is('login') || Request::is('register')) {{ 'navbar-dark' }} @endif"> {{-- @if (Route::has('login') || Route::has('register')) {{ 'navbar-dark' }} @endif --}}
+<nav class="navbar navbar-expand-lg @if (!Request::is('/')) {{ 'navbar-dark' }} @endif"> {{-- @if (Route::has('login') || Route::has('register')) {{ 'navbar-dark' }} @endif --}}
     <div class="container">
 
         <!-- Navbar Logo -->
         <a class="navbar-brand" href="{{ route('portfolio.home.index') }}">
-            <img src="@if (Request::is('login') || Request::is('register')) {{ asset('assets/img/logo-dark.png') }} @else{{ asset('assets/img/logo-light.png') }} @endif" alt="Revex">
+            <img src="@if (!Request::is('/')) {{ asset('assets/img/logo-dark.png') }} @else{{ asset('assets/img/logo-light.png') }} @endif" alt="Revex">
         </a>
 
         <!-- Toggler/collapsibe Button -->
