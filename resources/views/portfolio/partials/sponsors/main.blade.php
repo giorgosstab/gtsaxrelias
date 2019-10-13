f3541b6acf6202078ae8f2d940c06b5d8d8b9468
Giorgos Tsaxrelias <tsaxre1991@hotmail.com>
2019-10-13 15:12:21 +0300

    Make sponsors in different file

---
+++ resources/views/portfolio/partials/sponsors/main.blade.php
@@ -0,0 +1,43 @@
+<section class="sponsors">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="content">
+                    <div class="owl-carousel owl-theme">
+
+                        <!-- Sponsors Items -->
+                        <div class="item">
+                            <img class="img-fluid" src="{{ asset('assets/img/sponsors/1.jpg') }}" alt="Sponsors" />
+                        </div>
+
+                        <div class="item">
+                            <img class="img-fluid" src="{{ asset('assets/img/sponsors/2.jpg') }}" alt="Sponsors" />
+                        </div>
+
+                        <div class="item">
+                            <img class="img-fluid" src="{{ asset('assets/img/sponsors/3.jpg') }}" alt="Sponsors" />
+                        </div>
+
+                        <div class="item">
+                            <img class="img-fluid" src="{{ asset('assets/img/sponsors/4.jpg') }}" alt="Sponsors" />
+                        </div>
+
+                        <div class="item">
+                            <img class="img-fluid" src="{{ asset('assets/img/sponsors/5.jpg') }}" alt="Sponsors" />
+                        </div>
+
+                        <div class="item">
+                            <img class="img-fluid" src="{{ asset('assets/img/sponsors/6.jpg') }}" alt="Sponsors" />
+                        </div>
+
+                        <div class="item">
+                            <img class="img-fluid" src="{{ asset('assets/img/sponsors/7.jpg') }}" alt="Sponsors" />
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
